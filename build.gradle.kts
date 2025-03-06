5df8da4746f5ab13b918133c0bc5730fe8b078eb
Seggan <seggan21@gmail.com>
2025-03-06 15:42:05 -0500

    Executable

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     kotlin("jvm") version "2.1.10"
+    id("com.gradleup.shadow") version "8.3.2"
+    application
 }
 
 group = "io.github.seggan"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -11,4 +13,16 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass = "io.github.seggan.imagealign.MainKt"
+}
+
+tasks.shadowJar {
+    archiveClassifier = ""
 }
